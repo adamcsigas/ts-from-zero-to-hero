617f4b9e390b6cc7bd3bd1cd00907ba3df47bb04
adamcsigas <csigasadam@gmail.com>
2021-04-03 22:41:57 +0200

    small introduction on ts core types: string, number, boolean

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = typeof number1 === 'number' && typeof number2 === 'number';
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
+console.log(result);
