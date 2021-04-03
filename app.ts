a5a41dd47403eaff7c0a139a39c7cc4211022f90
adamcsigas <csigasadam@gmail.com>
2021-04-03 23:05:28 +0200

    fix typo and add a small extra note to type assignments

--- app.ts
+++ app.ts
@@ -12,13 +12,15 @@ function add(
   }
 }
 
-// The reason why we don't use type assignments here is because typescript has a built in function called type interference
+// The reason why we don't use type assignments here is because typescript has a built in function called type inference
 // this means typescript does it's best to understand which type you have in a certain variable or const
 // for instance initialize the number1 variable:
 // with const number1 = 5, it's type going to be not just any number but :5
 // if you use let number1 = 5 it's goint to be :number
-// and of course you could give it's type like let number1: number = 5
+// and of course you could give it's type explicitly like let number1: number = 5 (the code will not rely on type inference
+// in this case)
 // but that is unnecessary since TS can perfectly tell what is the type is going to be (reduntant code)
+
 const number1 = 5;
 const number2 = 2.8;
 const printResult = typeof number1 === 'number' && typeof number2 === 'number';
