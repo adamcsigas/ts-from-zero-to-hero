f2e0f008df35b797e9b7958e36631585f04bab3c
adamcsigas <csigasadam@gmail.com>
2021-04-04 14:15:10 +0200

    - added some notes on any type - separated app.ts content into objs-arrays-enum - represent union types

--- app.ts
+++ app.ts
@@ -1,81 +1,27 @@
-/* this approach is only for demo purpose it's not best practice!
-L2-5 is a typescript representation of an object's type
-by default it's better let type interference do it's thing :)
- const person : {
-  name: string;
-  age: number;
-} = {
-  name: 'Adam',
-  age: 29,
-}; */
+//type union:
+//let's say we want to add two values. not only numbers but strings as well (like concat)
+//in this case we can use union type: number | string for instance
 
-/* const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //tuple concept: added by TS: fixed-length array
-} = {
-  name: 'Adam',
-  age: 29,
-  hobbies: [
-    'sports',
-    'cooking',
-  ],
-  role: [2, 'author'],
-}; */
+//note: TS doesn't analyze what the union types contain, only recognize that it is a union type
+//so if you use const result = input1 + input2; for instance even though the + operator can be
+//used on string and number, it will throw an error because from TS point of view
+//it might contain things that cannot be added together
+//in case needed, you can use a runtime check to overcome this issue
 
-//this is a common pattern in JS to store eg. roles in consts
-//but it has downsides like in the person object we can use any number in the role
-//even ones that are not exists
-//and in addition we have to define all these constants and manage them
+function combine(input1: number | string, input2: number | string) {
+  let result;
 
-//const ADMIN = 0;
-//const READ_ONLY = 1;
-//const AUTHOR = 2;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
 
-//enum makes this easier! enum is a custom type
-enum Role { ADMIN, READ_ONLY, AUTHOR }; 
-//behind the scenes every element recieves an index by default ADMIN the number 0, READ_ONLY the number 1 and so on
-//if you need different behaviour you can change the number simply: ADMIN = 5. 
-//after that the upcoming element will be 6 and so on
-
-const person = {
-  name: 'Adam',
-  age: 29,
-  hobbies: [
-    'sports',
-    'cooking',
-  ],
-  role: Role.ADMIN,
-};
-
-//by default we could do
-//person.role.push('admin'); or person.role[1] = 10;
-//because typescript doesn't know that we want only 2 elements
-//typescript interference works it says role can be a type of string | number[]
-//but we explicitly want to override it
-//with tuple types of the elements and the length is fixated
-
-let favoriteActivities: string[];
-favoriteActivities = ['sports'];
-
-console.log(person.name);
-
-//typescript interference can tell that a hobby is going to be a string, so all
-//built in string function will be available
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  //but for instance we would get an error on console.log(hobby.map());
+  return result;
 }
 
-if(person.role === Role.ADMIN) {
-  console.log('admin');
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-//some notes on the last type which was mentioned: any
-//although it sounds good at first but you want avoid the type any
-//anywhere it's possible because it has a great disadvantage to take
-//away TS strictness about types. It can be used as a fallback when
-//you really can't know what kind of data will be stored in there
-//and then you can maybe use some runtime checks
-//by default: you want to avoid using any by any mean!!! .-)
+const combinedNames = combine('Adam', 'Yolo');
+console.log(combinedNames);
---
+++ objs-arrays-enum.ts
@@ -0,0 +1,81 @@
+/* this approach is only for demo purpose it's not best practice!
+L2-5 is a typescript representation of an object's type
+by default it's better let type interference do it's thing :)
+ const person : {
+  name: string;
+  age: number;
+} = {
+  name: 'Adam',
+  age: 29,
+}; */
+
+/* const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //tuple concept: added by TS: fixed-length array
+} = {
+  name: 'Adam',
+  age: 29,
+  hobbies: [
+    'sports',
+    'cooking',
+  ],
+  role: [2, 'author'],
+}; */
+
+//this is a common pattern in JS to store eg. roles in consts
+//but it has downsides like in the person object we can use any number in the role
+//even ones that are not exists
+//and in addition we have to define all these constants and manage them
+
+//const ADMIN = 0;
+//const READ_ONLY = 1;
+//const AUTHOR = 2;
+
+//enum makes this easier! enum is a custom type
+enum Role { ADMIN, READ_ONLY, AUTHOR }; 
+//behind the scenes every element recieves an index by default ADMIN the number 0, READ_ONLY the number 1 and so on
+//if you need different behaviour you can change the number simply: ADMIN = 5. 
+//after that the upcoming element will be 6 and so on
+
+const person = {
+  name: 'Adam',
+  age: 29,
+  hobbies: [
+    'sports',
+    'cooking',
+  ],
+  role: Role.ADMIN,
+};
+
+//by default we could do
+//person.role.push('admin'); or person.role[1] = 10;
+//because typescript doesn't know that we want only 2 elements
+//typescript interference works it says role can be a type of string | number[]
+//but we explicitly want to override it
+//with tuple types of the elements and the length is fixated
+
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
+
+console.log(person.name);
+
+//typescript interference can tell that a hobby is going to be a string, so all
+//built in string function will be available
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //but for instance we would get an error on console.log(hobby.map());
+}
+
+if(person.role === Role.ADMIN) {
+  console.log('admin');
+}
+
+//some notes on the last type which was mentioned: any
+//although it sounds good at first but you want avoid the type any
+//anywhere it's possible because it has a great disadvantage to take
+//away TS strictness about types. It can be used as a fallback when
+//you really can't know what kind of data will be stored in there
+//and then you can maybe use some runtime checks
+//by default: you want to avoid using any by any mean!!! .-)
