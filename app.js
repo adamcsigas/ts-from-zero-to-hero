80a18f973e05f773845e62233150bbebb0c72a5e
adamcsigas <csigasadam@gmail.com>
2021-04-04 12:27:34 +0200

    introducing the concept of the type tuple

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* this approach is only for demo purpose it's not best practice!
 L2-5 is a typescript representation of an object's type
+by default it's better let type interference do it's thing :)
  const person : {
   name: string;
   age: number;
@@ -7,19 +8,25 @@ L2-5 is a typescript representation of an object's type
   name: 'Adam',
   age: 29,
 }; */
-//it's better let type interference do it's thing :)
 var person = {
     name: 'Adam',
     age: 29,
     hobbies: [
         'sports',
         'cooking',
-    ]
+    ],
+    role: [2, 'author']
 };
+//by default we could do
+//person.role.push('admin'); or person.role[1] = 10;
+//because typescript doesn't know that we want only 2 elements
+//typescript interference works it says role can be a type of string | number[]
+//but we explicitly want to override it
+//with tuple types of the elements and the length is fixated
 var favoriteActivities;
 favoriteActivities = ['sports'];
 console.log(person.name);
-//typescript interference says that a hobby is going to be a string, so all
+//typescript interference can tell that a hobby is going to be a string, so all
 //built in string function will be available
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 /* this approach is only for demo purpose it's not best practice!
 L2-5 is a typescript representation of an object's type
+by default it's better let type interference do it's thing :)
  const person : {
   name: string;
   age: number;
@@ -8,22 +9,34 @@ L2-5 is a typescript representation of an object's type
   age: 29,
 }; */
 
-//it's better let type interference do it's thing :)
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //tuple concept
+} = {
   name: 'Adam',
   age: 29,
   hobbies: [
     'sports',
     'cooking',
   ],
+  role: [2, 'author'],
 };
+
+//by default we could do
+//person.role.push('admin'); or person.role[1] = 10;
+//because typescript doesn't know that we want only 2 elements
+//typescript interference works it says role can be a type of string | number[]
+//but we explicitly want to override it
+//with tuple types of the elements and the length is fixated
 
 let favoriteActivities: string[];
 favoriteActivities = ['sports'];
 
 console.log(person.name);
 
-//typescript interference says that a hobby is going to be a string, so all
+//typescript interference can tell that a hobby is going to be a string, so all
 //built in string function will be available
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
