c231b41d9efbd48b728fedc1e8a0ede704750b61
adamcsigas <csigasadam@gmail.com>
2021-04-08 01:22:33 +0200

    - added some extra notes on interfaces - separated interfaces into section5/interfaces.ts

---
+++ dist/interfaces.js
@@ -0,0 +1,36 @@
+"use strict";
+//Interface:
+//----------
+//describe how an object should look like
+//without concrete values.
+//it might look like an abstract class
+//but in interface you don't have any concrete
+//thing where as in abstract class you might have
+//some concrete implementation for a method
+//or have a concrete value for a property
+var Person = /** @class */ (function () {
+    function Person(n) {
+        this.age = 29;
+        if (n) {
+            this.name = n;
+        }
+    }
+    Person.prototype.greet = function (phrase) {
+        console.log(phrase + " " + this.name);
+    };
+    return Person;
+}());
+var user1;
+user1 = new Person('Adam');
+//user1.name = 'Tadam'; this will throw error because it's a readonly prop in the interface
+user1.greet('Hi there - I am');
+var add;
+add = function (a, b) {
+    return a + b;
+};
+//Good to know notes:
+//Interfaces a TS only/pure developement feature!
+//You can use interface to improve your code
+//but no output will end up in your JS files though!
+//it's been used during compilation to check your code
+//then it's dumped. :(
