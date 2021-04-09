d7d4ccc7ffe118e625b062834f9c59d64b5b8230
adamcsigas <csigasadam@gmail.com>
2021-04-09 14:21:51 +0200

    introduce different type of decorators through examples: - property decorator - accessor decorator - method decorator - parameter decorator

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 //DECORATOR-FACTORY:
 /*
   returns a decorator function
@@ -54,5 +57,72 @@ console.log(pers);
 the factory functions. Logger --> WithTemplate
 - but the execution of the actual decorator functions then happens
 bottom up! WithTemplate --> Logger
-
 */
+/*
+  if you add decorator to a property, decorator will take 2 arguments
+  1.)
+    a.)target property:
+    in case of instance property (like the example below), target will refer
+    the prototype of the object if it was created
+    b.)static property:
+    target will refer to the constructor function
+    Because we don't know the structure what this object will have,
+    target will be any type this time
+  2.)property name
+*/
+//property decorator
+function Log(target, propertyName) {
+    console.log('Property decorator');
+    console.log(target, propertyName);
+}
+//accessor decorator
+function Log2(target, name, descriptor) {
+    console.log('Accessor decorator!');
+    console.log(target); //prototype in this case
+    console.log(name); //name of the accessor itself (not the internal value!!!)
+    console.log(descriptor); //property descriptor
+}
+//method decorator
+//recieve the same args as accessor decorator
+function Log3(target, name, descriptor) {
+    console.log('Method decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor); // is a little bit different then accessor descriptor, not TS specific though
+}
+//parameter decorator
+//this gets the name of the method where this parameter is being used!
+//position: the position of the argument in the method where used
+function Log4(target, name, position) {
+    console.log('Parameter decorator');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
--- src/app.ts
+++ src/app.ts
@@ -51,5 +51,76 @@ console.log(pers);
 the factory functions. Logger --> WithTemplate
 - but the execution of the actual decorator functions then happens
 bottom up! WithTemplate --> Logger
-
 */
+
+/*
+  if you add decorator to a property, decorator will take 2 arguments
+  1.)
+    a.)target property:
+    in case of instance property (like the example below), target will refer
+    the prototype of the object if it was created
+    b.)static property:
+    target will refer to the constructor function
+    Because we don't know the structure what this object will have,
+    target will be any type this time
+  2.)property name
+*/
+
+//property decorator
+function Log(target: any, propertyName: string | symbol) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
+
+//accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator!');
+  console.log(target); //prototype in this case
+  console.log(name); //name of the accessor itself (not the internal value!!!)
+  console.log(descriptor); //property descriptor
+}
+
+//method decorator
+//recieve the same args as accessor decorator
+function Log3 (target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor); // is a little bit different then accessor descriptor, not TS specific though
+}
+
+//parameter decorator
+//this gets the name of the method where this parameter is being used!
+//position: the position of the argument in the method where used
+function Log4 (target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  constructor (t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log2
+  set price(val: number) {
+    if(val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
