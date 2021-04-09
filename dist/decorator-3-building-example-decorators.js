56030cefc64c755f8d0d7bebc9759a4537abffd8
adamcsigas <csigasadam@gmail.com>
2021-04-09 19:13:00 +0200

    - added some notes to decorators - separated decorator into section8/ directory

---
+++ dist/decorator-3-building-example-decorators.js
@@ -0,0 +1,244 @@
+"use strict";
+/*
+In this file you find example codes and notes to:
+  - Returning and changing a Class in a class decorator
+  - Creating an "Autobind" Decorator
+  - Validation with Decorators
+*/
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+function Logger(logString) {
+    console.log('logger factory');
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+//Returning and changing a Class in a class decorator
+/*
+  with the following modification the decorator will only run
+  if the class where the decorator is being used instantiated
+*/
+function WithTemplate(template, hookId) {
+    console.log('template factory');
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            //this based on my original constructor function(aka CF)
+            //with the following logic I replace the original class
+            //with a custom class with extra logic, which not run
+            //when the class is defined, but when instantiated
+            constructor(..._) {
+                super();
+                console.log('rendering template');
+                const hookEl = document.getElementById(hookId);
+                //const p = new originalConstructor();
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent = this.name;
+                }
+            }
+        };
+    };
+}
+//Explaining <T extends {new(...args: any[]): {name: string}}> <--- this magic:
+/*
+  - We want to say that our decorator function going to be a generic fn:
+    <T>
+
+  - That generic fn will accept such object,
+    which we can make new instances:
+    <T extends {new()}>
+
+  - This object can accept as many arguements as we want:
+    <T extends {new(...args: any[])}>
+
+  - The generic fn at the end will return with an object:
+    <T extends {new(...args: any[])}: {}>
+
+  - We have to make clear the returning object will have
+    a name property with the type string:
+    <T extends { new(...args: any[]): { name: string } }>
+*/
+let Person = class Person {
+    constructor() {
+        this.name = 'Adam';
+        console.log('Creating person object...');
+    }
+};
+Person = __decorate([
+    Logger('Logging...'),
+    WithTemplate('<h1>My Person Object</h1>', 'app')
+], Person);
+const pers = new Person();
+console.log(pers);
+//property decorator
+function Log(target, propertyName) {
+    console.log('Property decorator');
+    console.log(target, propertyName);
+}
+//accessor decorator
+function Log2(target, name, descriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+//method decorator
+function Log3(target, name, descriptor) {
+    console.log('Method decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+//parameter decorator
+function Log4(target, name, position) {
+    console.log('Parameter decorator');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
+/*
+  You can return values on the following decorators:
+  - class decorator
+  - method decorators - Log3
+  - accessor decorators - Log2
+*/
+//Creating an "Autobind" Decorator
+//--------------------------------
+function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            //What does "this" refers to, here?
+            //TLDR; we ensure that "this" will always refer to the exact same object
+            //as the original method
+            /*
+              We are inside of the getter method, so "this" will
+              refer to whatever is responsible for triggering the get()
+              method.
+              This is the trick: getter method will be triggered
+              by the concrete object to which it's belongs. So
+              "this" inside of the getter method will always refer
+              to the object on which we defined the get().
+      
+              This will not overwritten by addEventListener, because
+              the getter is like an extra layer between our function
+              that's being executed, the object to which it belongs
+              and the eventListener.
+            */
+            return boundFn;
+        },
+    };
+    return adjustedDescriptor;
+}
+class Printer {
+    constructor() {
+        this.message = 'This works!';
+    }
+    showMessage() {
+        console.log(this.message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector('button');
+button.addEventListener('click', p.showMessage);
+const registeredValidators = {};
+function Require(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: [...registeredValidators[target.constructor.name][propName], 'required'] });
+}
+function PositiveNumber(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: [...registeredValidators[target.constructor.name][propName], 'positive'] });
+}
+function validate(obj) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for (const validator of objValidatorConfig[prop]) {
+            console.log(prop);
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+class Course {
+    constructor(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+}
+__decorate([
+    Require
+], Course.prototype, "title", void 0);
+__decorate([
+    PositiveNumber
+], Course.prototype, "price", void 0);
+const courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title');
+    const priceEl = document.getElementById('price');
+    const title = titleEl.value;
+    const price = +priceEl.value; //+ cast it to a number
+    const createdCourse = new Course(title, price);
+    if (!validate(createdCourse)) {
+        alert('Invalid input, please try again!');
+        return;
+    }
+    console.log(createdCourse);
+});
+/*
+decorator related libraries/frameworks to look at:
+- https://github.com/typestack/class-validator
+- https://angular.io/
+- https://nestjs.com/
+*/
---
+++ src/section8-decorators/decorator-3-building-example-decorators.ts
@@ -0,0 +1,282 @@
+/*
+In this file you find example codes and notes to:
+  - Returning and changing a Class in a class decorator
+  - Creating an "Autobind" Decorator
+  - Validation with Decorators
+*/
+
+function Logger(logString: string) {
+  console.log('logger factory');
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+//Returning and changing a Class in a class decorator
+/*
+  with the following modification the decorator will only run
+  if the class where the decorator is being used instantiated
+*/
+function WithTemplate(template: string, hookId: string) {
+  console.log('template factory');
+  return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+    return class extends originalConstructor {
+      //this based on my original constructor function(aka CF)
+      //with the following logic I replace the original class
+      //with a custom class with extra logic, which not run
+      //when the class is defined, but when instantiated
+      constructor(..._: any) {
+        super();
+        console.log('rendering template');
+        const hookEl = document.getElementById(hookId);
+        //const p = new originalConstructor();
+        if(hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    }
+  }
+}
+//Explaining <T extends {new(...args: any[]): {name: string}}> <--- this magic:
+/*
+  - We want to say that our decorator function going to be a generic fn:
+    <T>
+
+  - That generic fn will accept such object,
+    which we can make new instances:
+    <T extends {new()}>
+
+  - This object can accept as many arguements as we want:
+    <T extends {new(...args: any[])}>
+
+  - The generic fn at the end will return with an object:
+    <T extends {new(...args: any[])}: {}>
+
+  - We have to make clear the returning object will have
+    a name property with the type string:
+    <T extends { new(...args: any[]): { name: string } }>
+*/
+
+@Logger('Logging...')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+  name = 'Adam';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
+
+//property decorator
+function Log(target: any, propertyName: string | symbol) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
+
+//accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+//method decorator
+function Log3 (target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+//parameter decorator
+function Log4 (target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  constructor (t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log2
+  set price(val: number) {
+    if(val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+/*
+  You can return values on the following decorators:
+  - class decorator
+  - method decorators - Log3
+  - accessor decorators - Log2
+*/
+
+//Creating an "Autobind" Decorator
+//--------------------------------
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this); 
+      //What does "this" refers to, here?
+      //TLDR; we ensure that "this" will always refer to the exact same object
+      //as the original method
+      /*
+        We are inside of the getter method, so "this" will
+        refer to whatever is responsible for triggering the get()
+        method.
+        This is the trick: getter method will be triggered
+        by the concrete object to which it's belongs. So
+        "this" inside of the getter method will always refer
+        to the object on which we defined the get().
+
+        This will not overwritten by addEventListener, because
+        the getter is like an extra layer between our function
+        that's being executed, the object to which it belongs
+        and the eventListener.
+      */
+      return boundFn;
+    },
+  }
+  return adjustedDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage); 
+//output: undefined
+/*
+  within addEventListener, if we point at a function
+  that should be executed (showMessage) the "this" keyword
+  inside of that function will not have the same
+  context/reference as we call just p.showMessage.
+  In this case "this" will refer to the target of the event!
+  Because addEventListener binds the "this" keyword
+  to the target of the event.
+  Common workaround: p.showMessage.bind(p)
+  You can also build a decorator to do this! (Autobind())
+*/
+
+//------------
+
+//validation with decorators
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[] //['require', 'positive']
+  }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Require(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [...registeredValidators[target.constructor.name][propName], 'required']
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [...registeredValidators[target.constructor.name][propName], 'positive']
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if(!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for(const validator of objValidatorConfig[prop]) {
+      console.log(prop);
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Require
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value; //+ cast it to a number
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert('Invalid input, please try again!');
+    return;
+  }
+
+  console.log(createdCourse);
+})
+
+/*
+decorator related libraries/frameworks to look at:
+- https://github.com/typestack/class-validator
+- https://angular.io/
+- https://nestjs.com/
+*/
